9703d07e9b82a31acc01f58dfbba603d5f62b2c4
Alex <alex@wendtedesigns.com>
2019-01-04 09:30:30 -0600

    updates site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wendte Designs`,
+    description: `Wendte Designs | Full Stack Development. React and Node.js.`,
+    author: `Alex Wendte`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
